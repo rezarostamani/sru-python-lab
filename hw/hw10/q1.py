484602867874106c5049a403c7f8ca819424160c
Soroush <soroush.gooran@gmail.com>
2018-12-24 08:23:50 +0330

    Update q1.py

--- hw/hw10/q1.py
+++ hw/hw10/q1.py
@@ -2,6 +2,10 @@ import numpy as np
 a = np.array([[0,0,0,1],[1,0,1,1],[1,0,0,1],[0,0,1,0]])
 k = int(input("Enter k:"))
 bk = np.zeros((4,4))
-for i in range(1, k+1):
-    bk += a**i
+b = np.eye(4)
+
+for i in range(k):
+    b = np.matmul(a, b)
+    bk += b 
+    
 print(bk)
